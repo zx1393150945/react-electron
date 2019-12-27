aff97197b1082bde985a42746e49d146a1571c8d
zx <1393150945@qq.com>
2019-12-27 14:39:40 +0800

    项目初始化

---
+++ main.js
@@ -0,0 +1,41 @@
+// Modules to control application life and create native browser window
+const {app, BrowserWindow} = require('electron')
+const isDev = require('electron-is-dev')
+const path = require('path')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+function createWindow () {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 1100,
+        height: 900,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    })
+
+    // and load the index.html of the app.
+    const url = isDev ? 'http://localhost:3000' : 'zxx'
+    mainWindow.loadURL(url)
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
+}
+
+app.on('ready', async () => {
+    createWindow()
+    require('devtron').install()
+})
+
+
+
+
+
+
+
+
+
+
