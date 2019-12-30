40d8a3fe2d3dc8ff2d981edcd75acda387a102b0
zx <1393150945@qq.com>
2019-12-30 09:47:15 +0800

    file-search

---
+++ config-overrides.js
@@ -0,0 +1,7 @@
+const {override, addLessLoader} = require('customize-cra');
+module.exports = override(
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars: {'@primary-color': '#1DA57A'},
+    })
+)
--- src/components/file-search/file-search.jsx
+++ src/components/file-search/file-search.jsx
@@ -1,4 +1,8 @@
 import React, {useState, useEffect, useRef} from 'react'
+import PropTypes from 'prop-types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch, faTimes } from '@fortawesome/free-solid-svg-icons'
+import './file-search.less'
 
 export const FileSearch = ({title, onFileSearch}) => {
     const [inputActive, setInputActive] = useState(false)
@@ -31,27 +35,36 @@ export const FileSearch = ({title, onFileSearch}) => {
        }
     }, [inputActive])
     // A && B  返回值 如果A是false,返回A 否则返回 B , 也就是哪个是假返回哪个
+    //   <> </> 在页面上是不会有什么节点的
     return (
-        <div className={"alert alert-primary"}>
+        <div className={"alert alert-primary d-flex justify-content-between align-items-center"}>
             {!inputActive &&
-              <div className={"d-flex justify-content-between align-items-center"}>
+                <>
                  <span>{title}</span>
                   <button type={"button"}
-                          className={"btn btn-primary"}
+                          className={"icon-button"}
                           onClick={() => setInputActive(true)}
-                  >搜索</button>
-              </div>
+                  >
+                      <FontAwesomeIcon icon={faSearch} title={"搜索"} size={"lg"}/>
+                  </button>
+                </>
             }
             {
                 inputActive &&
-                <div className={"row"}>
-                    <input ref={input} type="text" className={"form-control col-8"} value={value} id="" onChange={ e => setValue(e.target.value) }/>
+                <>
+                    <input ref={input} style={{height:'25px'}} type="text" className={"form-control"} value={value} id="" onChange={ e => setValue(e.target.value) }/>
                     <button type={"button"}
-                            className={"btn btn-primary col-4"}
+                            className={"icon-button"}
                             onClick={closeSearch}
-                    >关闭</button>
-                </div>
+                    >
+                        <FontAwesomeIcon icon={faTimes} title={"关闭"} size={"lg"}/>
+                    </button>
+                </>
             }
         </div>
     )
+}
+FileSearch.propTypes = {
+  title: PropTypes.string.isRequired,
+  onFileSearch: PropTypes.func.isRequired
 }
