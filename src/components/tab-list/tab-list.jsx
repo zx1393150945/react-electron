d9234ac73c488ecb43d4e75268dc8507851f7f53
zx <1393150945@qq.com>
2019-12-31 14:00:36 +0800

    Bottom-btn

---
+++ src/components/tab-list/tab-list.jsx
@@ -0,0 +1,48 @@
+import React, {useState, useEffect} from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faTimes} from '@fortawesome/free-solid-svg-icons'
+import './tab-list.less'
+export const TabList = ({files, activeId, unsavedIds, onTabClick, onCloseTab}) => {
+  return (
+      <ul className={"nav nav-pills tab-list"}>
+          {files.map(file =>{
+              const withUnsaveMark = unsavedIds.includes(file.id)
+              const fclassname = classNames({
+                  'nav-link ': true,
+                  active: file.id === activeId,
+                  withUnsaved: withUnsaveMark
+              })
+
+             return  (
+                 <li className={"nav-item"} key={file.id}>
+                     <a href="" className={fclassname} onClick={(e) => {e.preventDefault(); onTabClick(file.id)}}>
+                         {file.title}
+                         <span className={"ml-2 close-icon"} onClick={e => {e.preventDefault();e.stopPropagation(); onCloseTab(file.id)}}>
+                             <FontAwesomeIcon icon={faTimes}/>
+                         </span>
+                         {
+                             withUnsaveMark ? (
+                                 <span className={"rounded-circle ml-2 unsaved-icon"}></span>) : null
+                         }
+                     </a>
+                 </li>
+             )
+          })}
+
+      </ul>
+  )
+}
+
+TabList.propTypes = {
+    files: PropTypes.array,
+    activeId: PropTypes.string,
+    unsavedIds: PropTypes.array,
+    onTabClick: PropTypes.func,
+    onCloseTab: PropTypes.func
+}
+
+TabList.defaultProps = {
+    unsavedIds: []
+}
--- src/util/default-files.js
+++ src/util/default-files.js
@@ -2,7 +2,7 @@ export const defaultFiles = [
     {
         id: '1',
         title: '文件1',
-        body: '这是第一个文件的内容',
+        body: '## 这是第一个文件的内容',
         createdAt: 1563762965704
     },
     {
